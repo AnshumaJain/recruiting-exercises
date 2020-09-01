4d12ac8625725d1323d44f3bad66711dc55bd38e
Anshuma <67174378+AnshumaJain@users.noreply.github.com>
2020-08-31 23:42:29 -0700

    upload Deliverr coding excercise

---
+++ inventory-allocator/src/inventory_allocator.py
@@ -0,0 +1,39 @@
+# Anshuma Jain
+# Problem: Compute the best way an order can be shipped (called shipments) given inventory across a set of warehouses (called inventory distribution).
+
+
+def update_shipment_distribution(shipment_distribution, warehouse_name, fruit, order_qty):
+    """ Updates the shipment distribution using the given warehouse_name, fruit, and order_qty """
+    if warehouse_name not in shipment_distribution:  # if warehouse doesn't exist in the shipment distribution list
+        shipment_distribution[warehouse_name] = {fruit: order_qty}  # Example entry: [ { owd: {apple:10} } ]
+    elif warehouse_name in shipment_distribution:
+        shipment_distribution[warehouse_name][fruit] = order_qty
+
+
+def inventory_allocator(order_list, inventory_distribution):
+    """ Creates a shipment_distribution based on the given order_list and inventory_distribution """
+    shipment_distribution = {}                      # to store the output
+    while order_list.keys():                        # iterate until order list is empty
+        fruit = list(order_list.keys())[0]          # select the first fruit from the order list
+        order_qty = order_list[fruit]               # obtain order qty for the given fruit
+
+        for warehouse_record in inventory_distribution:                     # loop thru each warehouse
+            warehouse_name = warehouse_record['name']
+            warehouse_inventory = warehouse_record['inventory']
+
+            if fruit in warehouse_inventory:                                # if the fruit exists in the warehouse inventory
+                if warehouse_inventory[fruit] >= order_qty:                 # if there are sufficient fruit qty in this warehouse's inventory
+                    update_shipment_distribution(shipment_distribution, warehouse_name, fruit, order_qty)
+                    warehouse_inventory[fruit] -= order_qty                 # adjust the warehouse inventory
+                    order_qty = 0
+                    order_list.pop(fruit)                                   # remove this fruit from the order list
+                    break                                                   # back to while loop for the next fruit
+                elif 0 < warehouse_inventory[fruit] < order_list[fruit]:    # if partial fruit qty in this warehouse
+                    update_shipment_distribution(shipment_distribution, warehouse_name, fruit, warehouse_inventory[fruit])
+                    order_qty -= warehouse_inventory[fruit]                 # compute remaining quantity
+                    warehouse_inventory[fruit] = 0
+
+        if order_qty != 0:  # if qty is still unfulfilled after going thru all the warehouses
+            return []
+
+    return [shipment_distribution]                                          # return a list
---
+++ inventory-allocator/test/inventory_allocator_test.py
@@ -0,0 +1,62 @@
+import sys
+import unittest
+
+sys.path.append("..")
+from src.inventory_allocator import inventory_allocator
+
+class TestInventoryAllocator(unittest.TestCase):
+
+
+    def test_inventory_allocator_single_warehouse(self):
+        # Order can be shipped using one warehouse
+        order_list = {'apple': 1}
+        inventory_distribution = [{'name': 'owd', 'inventory': {'apple': 1}}]
+        expected_result = [{'owd': {'apple': 1}}]
+
+        result = inventory_allocator(order_list, inventory_distribution)
+        self.assertEqual(result, expected_result)
+
+
+    def test_inventory_allocator_multiple_warehouse(self):
+        # Order can be shipped using multiple warehouses
+
+        order_list = {'apple': 10}
+        inventory_distribution = [{'name': 'owd', 'inventory': {'apple': 5}}, {'name': 'dm', 'inventory': {'apple': 5}}]
+        expected_result = [{'dm': {'apple': 5}, 'owd': {'apple': 5}}]
+
+        result = inventory_allocator(order_list, inventory_distribution)
+        self.assertEqual(result, expected_result)
+
+
+    def test_inventory_allocator_not_enough_inventory(self):
+        # Order cannot be shipped because there is not enough inventory
+        order_list = {'apple': 1}
+        inventory_distribution = [{'name': 'owd', 'inventory': {'apple': 0}}]
+        expected_result = []
+
+        result = inventory_allocator(order_list, inventory_distribution)
+        self.assertEqual(result, expected_result)
+
+
+    def test_inventory_allocator_many_fruits(self):
+        # Many fruits in the order list
+        order_list = {'apple': 10, 'mango': 12, 'guava': 2, 'grapes': 1}
+        inventory_distribution = [
+                                    {'name': 'owd',
+                                     'inventory': {'apple': 10, 'mango': 5, 'grapes': 3}
+                                    },
+                                    {'name': 'dm',
+                                     'inventory': {'apple': 5, 'mango': 15, 'guava': 8}
+                                    }
+                                 ]
+        expected_result = [
+                            {'owd': {'apple': 10, 'mango': 5, 'grapes': 1},
+                             'dm': {'mango': 7, 'guava': 2}}
+                          ]
+
+        result = inventory_allocator(order_list, inventory_distribution)
+        self.assertEqual(result, expected_result)
+
+
+if __name__ == "__main__":
+    unittest.main()
